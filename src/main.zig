9160b098a7ccdbd87ab21d561957c9f5a4c25fdc
Koustubh Saxena <46974369+KotUW@users.noreply.github.com>
2024-06-16 03:50:32 +0100

    get arg parsung working

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,22 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var args = try std.process.argsWithAllocator(alloc);
+    defer args.deinit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const prog_name = args.next().?;
+    const sub_cmd = args.next();
 
-    try bw.flush(); // don't forget to flush!
-}
+    if (sub_cmd == null) {
+        std.debug.print("[ERROR] Invalid Subcommand\nUsage: {s} Subcommand\n", .{prog_name});
+        return;
+    }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    if (std.zig.c_builtins.__builtin_strcmp(sub_cmd.?, "sync") == 0) {
+        std.debug.print("cmd = sync", .{});
+    }
 }
